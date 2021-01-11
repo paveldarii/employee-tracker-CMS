63f0667cb68b84ed89b054f07394b1ccb42e2a6f
Pavel Darii <paveldarii@yahoo.com>
2021-01-10 19:26:54 -0500

    created the connection between server.js and mysql db

---
+++ server.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "employeeDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
