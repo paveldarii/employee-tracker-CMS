aeca53cb4d2a2a9ef9f9db1189995d9e23f122e2
Pavel Darii <paveldarii@yahoo.com>
2021-01-10 21:24:19 -0500

    created a function that prompts heme choices

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
+const DBName = "employeeDB";
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "employeeDB",
+  database: DBName,
 });
 
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  promptMainMenu();
 });
 
-inquirer
-  .prompt([])
-  .then((answers) => {})
-  .catch((error) => {
-    console.log(error);
-  });
+function promptMainMenu() {
+  inquirer
+    .prompt([
+      {
+        name: "toDo",
+        type: "list",
+        message: `You are currently in ${DBName} database main menu.\n  What action do you intend to perform?`,
+        choices: ["Create", "Retrieve", "Update", "Delete"],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.toDo) {
+        case "Create":
+          return console.log("Create");
+        case "Retrieve":
+          return console.log("Retrieve");
+        case "Update":
+          return console.log("Update");
+        case "Delete":
+          return console.log("Delete");
+        default:
+          console.log("error");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
