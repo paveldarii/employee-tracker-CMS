6c54be122bcfc4824d7315ff7ba5126a4bf15844
Pavel Darii <paveldarii@yahoo.com>
2021-01-10 22:07:10 -0500

    added four functions that will be called from the main menu

--- server.js
+++ server.js
@@ -27,15 +27,160 @@ function promptMainMenu() {
     .then((answer) => {
       switch (answer.toDo) {
         case "Create":
-          return console.log("Create");
+          return promptOptionsForCreate();
         case "Retrieve":
-          return console.log("Retrieve");
+          return promptOptionsForRetrieve();
         case "Update":
-          return console.log("Update");
+          return promptOptionsForUpdate();
         case "Delete":
           return console.log("Delete");
         default:
           console.log("error");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function promptOptionsForCreate() {
+  inquirer
+    .prompt([
+      {
+        name: "toCreate",
+        type: "list",
+        message: "What do you want to create?",
+        choices: [
+          "Create New Employee",
+          "Create New Department",
+          "Create New Role",
+          "Create New Manager",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.toCreate) {
+        case "Create New Employee":
+          return console.log("Create New Employee");
+        case "Create New Department":
+          return console.log("Create New Department");
+        case "Create New Role":
+          return console.log("Create New Role");
+        case "Create New Manager":
+          return console.log("Create New Manager");
+        case "Return to Main Menu":
+          return promptMainMenu();
+        default:
+          console.log("error");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function promptOptionsForRetrieve() {
+  inquirer
+    .prompt([
+      {
+        name: "toRetrieve",
+        type: "list",
+        message: "What do you want to create?",
+        choices: [
+          "Retrieve All Employees",
+          "Retrieve All Departments",
+          "Retrieve All Roles",
+          "Retrieve All Managers",
+          "Retrieve All Employees by Manager Id",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.toRetrieve) {
+        case "Retrieve All Employees":
+          return console.log("Create New Employee");
+        case "Retrieve All Departments":
+          return console.log("Create New Department");
+        case "Retrieve All Roles":
+          return console.log("Create New Role");
+        case "Retrieve All Managers":
+          return console.log("Create New Manager");
+        case "Retrieve All Employees by Manager Id":
+          return console.log("Retrieve All Employees by Manager Id");
+        case "Return to Main Menu":
+          return promptMainMenu();
+        default:
+          console.log("error from retrieve function");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function promptOptionsForUpdate() {
+  inquirer
+    .prompt([
+      {
+        name: "toUpdate",
+        type: "list",
+        message: "What do you want to Update?",
+        choices: [
+          "Update an Employee",
+          "Update a Department",
+          "Update a Roles",
+          "Update a Manager",
+          "Return to main menu",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.toRetrieve) {
+        case "Update an Employee":
+          return console.log("Update an Employee");
+        case "Update a Department":
+          return console.log("Update a Department");
+        case "Update a Roles":
+          return console.log("Update a Roles");
+        case "Update a Manager":
+          return console.log("Update a Manager");
+        case "Return to Main Menu":
+          return promptMainMenu();
+        default:
+          console.log("Error from retrieve function");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+function promptOptionsForDelete() {
+  inquirer
+    .prompt([
+      {
+        name: "toUpdate",
+        type: "list",
+        message: "What do you want to Update?",
+        choices: [
+          "Delete an Employee",
+          "Delete a Department",
+          "Delete a Roles",
+          "Delete a Manager",
+          "Return to main menu",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.toRetrieve) {
+        case "Delete an Employee":
+          return console.log("Update an Employee");
+        case "Delete a Department":
+          return console.log("Update a Department");
+        case "Delete a Roles":
+          return console.log("Update a Roles");
+        case "Delete a Manager":
+          return console.log("Update a Manager");
+        case "Return to Main Menu":
+          return promptMainMenu();
+        default:
+          console.log("Error from delete function.");
       }
     })
     .catch((error) => {
